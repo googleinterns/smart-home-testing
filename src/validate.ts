e7b472e4ef939082924d95aa7971d146df315e3f
jolene-loz <lozanojolene@gmail.com>
2020-07-24 17:40:41 -0400

    Cleaned up console logs and changed how requests were generated

--- src/validate.ts
+++ src/validate.ts
@@ -10,9 +10,13 @@ const sync_response_schema = require('../intents/sync.response.schema.json');
 const apiresponse = require('./apiresponse.json');
 
 
-const testsPassed = 0
-const testsFailed = 0
-const totalTests = 0
+var testsPassed = 0
+var testsFailed = 0
+var totalTests = 0
+
+/**
+ * Generate requests to validate the responses later on  
+*/
 
 function generateRandom(min, max){
     //helper function for obtaining a random requestId
@@ -20,37 +24,86 @@ function generateRandom(min, max){
 }
 
 function generateSyncReq(){
-    const requestId = generateRandom(100,999);
+    const requestRandId = generateRandom(100,999);
     const intent = "action.devices.SYNC"
-    return {
-    "requestId": requestId,
-    "inputs": [{
-      "intent": intent,
-       }]
-    }
+    
+    var syncReq = {};
+    syncReq.requestId = requestRandId;
+    syncReq.inputs = [];
+    syncReq.inputs.unshift({});
+    syncReq.inputs[0]["intent"] = intent;
+    return syncReq
+
 }
 
 const sync_request = generateSyncReq();
-const requestId = sync_request["requestId"];
+const requestRandId = sync_request["requestId"];
 
 function generateQueryReq(deviceID){
+    
+    const intent = "action.devices.QUERY";
+    var queryReq = {};
+    queryReq.requestId = requestRandId;
+    queryReq.inputs = [];
+    queryReq.inputs.unshift({});
+    queryReq.inputs[0]["intent"] = intent;
+    queryReq.payload = {};
+    queryReq.payload.devices = [];
+    queryReq.payload.devices.unshift({})
+    queryReq.payload.devices[0]['id'] = deviceID
+    
+    //add custom data so that the user can input this instead
+     
+    return queryReq
+    
+    
+//     return {
+//     "requestId": requestId,
+//     "inputs": [{
+//       "intent": "action.devices.QUERY",
+//       "payload": {
+//         "devices": [{
+//           "id": deviceID,
+//           "customData": {
+//             "fooValue": 74,
+//             "barValue": true,
+//             "bazValue": "foo"
+//           }
+//         }, {
+//           "id": "456",
+//           "customData": {
+//             "fooValue": 12,
+//             "barValue": false,
+//             "bazValue": "bar"
+//           }
+//         }]
+//       }
+//     }]
+//   }
+}
+
+const query_request = generateQueryReq("123");
+
+console.log(query_request)
+const deviceInfo = (query_request["inputs"][0]["payload"]);
+
+function generateExecuteReq(traits, attributes){
     return {
-    "requestId": requestId,
-    "inputs": [{
-      "intent": "actions.devices.QUERY",
-      "payload": {
-        "devices": [{
-          "id": deviceID,
-          "customData": {
-            "fooValue": 74,
-            "barValue": true,
-            "bazValue": "foo"
+        "requestId": requestId,
+        "inputs": [{
+          "intent": "action.devices.EXECUTE",
+          "payload": {
+            "commands": deviceInfo,
+              "execution": [{
+                "command": traits,
+                "params": {
+                  "on": true
+                }
+              }]
           }
         }]
       }
-    }]
-  }
-}
+    }
 
 
 function generateDisconnectReq(){
@@ -62,33 +115,25 @@ function generateDisconnectReq(){
     }
 }
 
-console.log(generateDisconnectReq());
-
 
 function validate(schema, apiresponse){
+    //this function will validate the responses from the requests 
     const validate = ajv.compile(schema);
     var valid = validate(apiresponse);
-    console.log("Testing: " + schema["title"]);
     if (!valid) {
         chai.assert.fail("😠 Test Failed because " +  ajv.errorsText(validate.errors));
-        //add actual vs. expected 
         testsFailed++;
     } else {
-        return "Test Successful!";
+        chai.assert.isOk(valid, "Test Successful!");
         testsPassed++;
     }
 }
 
 
 exports.displayTestResults = function displayTestResults(){
-    console.log('Total tests run: ${totalTests}')
+    totalTests = testsPassed + testsFailed;
+    return totalTests;
     
 }
-
-var data = [{one:1}, {two:2}, {three:3}];
-
-var values = data.map(o=>Object.values(o)[0]);
-
-console.log(values);
 
 
