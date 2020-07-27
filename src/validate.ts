1481555c4af3cd9abe2f0fa295d60a3007a958fe
jolene-loz <lozanoja@bc.edu>
2020-07-27 17:18:28 -0400

    removed quotes

--- src/validate.ts
+++ src/validate.ts
@@ -13,7 +13,7 @@ function generateSyncReq(){
     return {
         requestId,
         "inputs": [{
-          "intent": "action.devices.SYNC"
+          "intent": action.devices.SYNC
             }]
     }
 }
