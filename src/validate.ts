63f6eab7ee3d53664e65b82284451445a3095971
jolene-loz <lozanoja@bc.edu>
2020-07-31 14:17:34 -0400

    Revised request functions for Sync, Query and Disconnect

--- src/validate.ts
+++ src/validate.ts
@@ -1,79 +1,53 @@
-'use strict';
 
-chai.use(require('chai-json-schema-ajv'));
+'use-strict';
 
-function generateRandom(min, max){
-    //helper function for obtaining a random requestId
-    return Math.floor(Math.random() * (max-min) + min).toString();
+
+function generateRequestID(min, max){
+        return Math.floor(Math.random() * (max-min) + min).toString()
 }
+    
 
-
-function generateSyncReq(){
-    const requestId = generateRandom(100,999);
+export function generatSyncRequest(){
+    const requestId = generateRequestID(100,999);
     return {
         requestId,
-        "inputs": [{
-          "intent": action.devices.SYNC
-            }]
-    }
-}
-
-const sync_request = generateSyncReq();
-const requestRandId = sync_request["requestId"];
-
-function generateQueryReq(deviceID){
-    
-    const intent = "action.devices.QUERY";
-    var queryReq = {};
-    queryReq.requestId = requestRandId;
-    queryReq.inputs = [];
-    queryReq.inputs.unshift({});
-    queryReq.inputs[0]["intent"] = intent;
-    queryReq.payload = {};
-    queryReq.payload.devices = [];
-    queryReq.payload.devices.unshift({})
-    queryReq.payload.devices[0]["id"] = deviceID;
-
-    
-    //  for (var i = 0; i <deviceID.length; i++){
-    //         queryReq.payload.devices[i]["id"] = deviceID[i];
-    //     }
-    //add custom data
-    
-    return queryReq;
-    
-    
-}
-
-const deviceInfo = (query_request["inputs"][0]["payload"]);
-
-function generateExecuteReq(traits, params){
-    return {
-        "requestId": requestId,
-        "inputs": [{
-          "intent": "action.devices.EXECUTE",
-          "payload": {
-            "commands": deviceInfo,
-              "execution": [{
-                "command": traits,
-                params;
-              }]
-          }
-        }]
-      }
-    }
-
-
-function generateDisconnectReq(){
-    return {
-        "requestId": sync_request["requestId"],
-        "inputs": [{
-          "intent": "action.devices.DISCONNECT",
+        inputs: [{
+          intent: "action.devices.SYNC"
         }]
     }
 }
 
 
+let deviceIDs: Array<string>;
 
+interface customDatas = {
+    [key: string]: string | number | boolean
+}
+
+export function generateQueryRequest(deviceIds, customData){
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.QUERY",
+          payload: {
+            devices: devicesIds.map((deviceId, index) =>
+                ({id: deviceId, customData: customData[1]}))
+            }
+        }]
+    }
+}
+      
+
+
+export function generateDisconnectRequest(){
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.DISCONNECT"
+        }]
+    }
+}
 
 
