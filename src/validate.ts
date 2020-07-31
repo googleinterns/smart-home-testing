2660087c169110e98a6d5683c3f920584622ee63
jolene-loz <lozanoja@bc.edu>
2020-07-31 16:54:23 -0400

    Added execute request

--- src/validate.ts
+++ src/validate.ts
@@ -24,7 +24,7 @@ interface customDatas = {
     [key: string]: string | number | boolean
 }
 
-export function generateQueryRequest(deviceIds, customData){
+export function generateQueryRequest(deviceIds, customDatas){
     const requestId = generateRequestID(100,999);
     return {
         requestId,
@@ -32,12 +32,34 @@ export function generateQueryRequest(deviceIds, customData){
           intent: "action.devices.QUERY",
           payload: {
             devices: devicesIds.map((deviceId, index) =>
-                ({id: deviceId, customData: customData[1]}))
+                ({id: deviceId, customData: customDatas[1]}))
             }
         }]
     }
 }
-      
+
+let commands: Array<string>;
+interface params = {
+    [key: string]: string | number | boolean
+}
+
+
+export function generateExecuteRequest(deviceIds, customDatas, commands, params){
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.EXECUTE",
+          payload: {
+            devices: deviceIds.map((deviceId, index) =>
+                ({id: deviceId, customData: customDatas[1]})),
+            execution: commands.map((command, index) =>
+                ({command: command, params: params[1]}))
+            }
+        }]
+    }
+}
+
 
 
 export function generateDisconnectRequest(){
