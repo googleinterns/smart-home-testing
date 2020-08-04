f66ae2807c36f64a256553060cf0f0e3fe5e770c
jolene-loz <lozanoja@bc.edu>
2020-08-04 12:00:16 -0400

    Adding validate function

--- src/validate.ts
+++ src/validate.ts
@@ -1,31 +1,120 @@
-// import {v4 as uuidv4} from 'uuid';
+'use-strict';
 
-'use strict';
+const Ajv = require('ajv');
 
-const Ajv = require('ajv'); 
-const ajv = new Ajv();
-const chai = require('chai');
-
-chai.use(require('chai-json-schema-ajv'));
-
-//calling the JSON schemas to throw assertion errors
-const sync_response_schema = require('../intents/sync.response.schema.json');
-const apiresponse = require('./apiresponse.json');
-
+/**
+ * Generates random request ID 
+ * @param min Minimum number
+ * @param max Maximum number 
+ * @returns Random number between the range of the min to max number specified
+ */
+function generateRequestID(min, max){
+     return Math.floor(Math.random() * (max-min) + min).toString()
+}
     
-function validate(schema,apiresponse) {
-    const validate = ajv.compile(schema);
-    var valid = validate(apiresponse);
-    console.log("Testing: " + schema["title"]);
-    if (!valid) {
-        chai.assert.fail("😠 Test Failed because " +  ajv.errorsText(validate.errors));
-        //add actual vs. expected 
+/**
+ * Generates SYNC request 
+ * @returns Specified format for SYNC intent request. 
+ */
+export function generateSyncRequest(){
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.SYNC"
+        }]
     }
-    console.log("Test Successful!")
+}
+
+type CustomData = {[key: string]: any}
+
+interface QueryRequest{
+    requestId: string;
+    inputs: {
+     intent: string; payload: { devices: { id: string; customData: CustomData; }[]; };
+    }[];
+}
+
+/**
+ * Generates QUERY request 
+ * @param deviceIds array of strings specifying the device deviceIds
+ * @param customData an array of data for the respective devices specified by the developer 
+ * @returns Specified format for QUERY intent request. 
+ */
+export function generateQueryRequest(deviceIds: string[],customData: CustomData[]) : QueryRequest{
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.QUERY",
+          payload: {
+            devices: deviceIds.map((deviceId, index) =>
+                ({id: deviceId, customData: customData[index]}))
+            }
+        }]
+    }
 }
 
 
-validate(sync_response_schema, apiresponse)
+type Params = {[key: string]: any}
+interface ExecuteRequest{
+    requestId: string;
+    inputs: { 
+     intent: string; 
+     payload: 
+     { devices: 
+        { id: string; 
+        customData: CustomData;}[];
+    execution: { command: string; params: Params; }[];
+    }; 
+    }[];
+}
+
+/**
+ * Generates EXECUTE request 
+ * @param deviceIds Array of strings specifying the device deviceIds
+ * @param customData Array of data for the respective devices specified by the developer 
+ * @param commands Array of strings specifying the commands 
+ * @param params Array of parameters based on the respective specified commands 
+ * @returns Specified format for EXECUTE intent request. 
+ */
+ 
+export function generateExecuteRequest(deviceIds: string[], customData: CustomData[], commands: string[], params: Params[]): ExecuteRequest{
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.EXECUTE",
+          payload: {
+            devices: deviceIds.map((deviceId, index) =>
+                ({id: deviceId, customData: customData[index]})),
+            execution: commands.map((command, index) =>
+                ({command: command, params: params[index]}))
+            }
+        }]
+    }
+}
+
+/**
+ * Generates DISCONNECT request 
+ * @returns Specified format for DISCONNECT intent request. 
+ */
+export function generateDisconnectRequest(){
+    const requestId = generateRequestID(100,999);
+    return {
+        requestId,
+        inputs: [{
+          intent: "action.devices.DISCONNECT"
+        }]
+    }
+}
+
+export function validate(api_response, schema){
+    let ajv = new Ajv();
+    let validate = ajv.compile(schema)
+    let valid = validate(api_response);
+    if (!valid) ajv.errorsText(validate.errors);
+}
 
 
 
