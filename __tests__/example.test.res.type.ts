b3e09206478cf1c2ecf876f30a52acd2847c902a
jolene-loz <lozanoja@bc.edu>
2020-08-13 17:29:16 -0400

    Test to show invalid response type

---
+++ __tests__/example.test.res.type.ts
@@ -0,0 +1,9 @@
+'use-strict';
+import * as testlib from '../src/validate';
+
+test('Sync Response fails due to invalid response type', async () => {
+  const exampleSyncRes = require('./example.sync.response.json');
+  const syncErrors = testlib.validate(exampleSyncRes, 'fail');
+  console.log(syncErrors);
+  expect(syncErrors && syncErrors.join('\n')).toBe(undefined);
+});
