c8c00477b107b30c4da640cf08701d0d71a58ce1
jolene-loz <lozanoja@bc.edu>
2020-08-13 15:59:44 -0400

    eslint fixes and camel case

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -2,15 +2,15 @@
 import * as testlib from '../src/validate';
 //Test passes
 test("Test 1: Sync Response passes", async() => {
-  const example_sync_res = require('./example.sync.response.json');
-  const noErrors = testlib.validate(example_sync_res, 'sync');
+  const exampleSyncRes = require('./example.sync.response.json');
+  const noErrors = testlib.validate(exampleSyncRes, 'sync');
   expect(noErrors).toBe(undefined);
 });
 
 //Test fails and shows errors
 test("Test 2: Sync Response fails", async() => {
-  const example_sync_res_fail = require('./example.sync.response.fail.json');
-  const syncErrors = testlib.validate(example_sync_res_fail, 'sync');
+  const exampleSyncResFail = require('./example.sync.response.fail.json');
+  const syncErrors = testlib.validate(exampleSyncResFail, 'sync');
   console.log(syncErrors);
   expect(syncErrors && syncErrors.join('\n')).toBe(undefined);
 });
