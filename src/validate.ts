11017ed3748d648c01ed2e55dfbbc055abeacbd2
jolene-loz <lozanoja@bc.edu>
2020-08-13 15:59:00 -0400

    JSDoc comments

--- src/validate.ts
+++ src/validate.ts
@@ -2,21 +2,32 @@
 import * as Ajv from 'ajv';
 
 const ajv = new Ajv({
-    allErrors:true
+  allErrors: true,
 });
 
-export function validateSyncResponse(apiResponse: object) {
-    const sync_response_schema = require('../intents/sync.response.schema.json')
-    const isValid = ajv.validate(sync_response_schema, apiResponse)
-    if (isValid) {
-        return undefined
-    }
-    return ajv.errors
+/**
+ * Helper function that uses AJV library to validate the response against the schema 
+ * @param apiResponse User defined api response
+ * @return Returns undefined if valid is true, returns an array of error(s) if valid is false. 
+ */
+function validateSyncResponse(apiResponse: object) {
+  const syncResponseSchema = require('../intents/sync.response.schema.json');
+  const isValid = ajv.validate(syncResponseSchema, apiResponse);
+  if (isValid) {
+    return undefined;
+  }
+  return ajv.errors;
 }
 
-export function validate(apiResponse: object, responseType:  'sync' | 'query'|'execute'| 'disconnect'){
-    if (responseType == 'sync'){
-        return validateSyncResponse(apiResponse);
-    }
-    return undefined;
+/**
+ * Identifies the response type and validates the function based on the schemas.
+ * @param apiResponse User defined api response 
+ * @param responseType User defined intent response 
+ * @return Errors from AJV validation, if any. Undefined otherwise. 
+ */
+export function validate(apiResponse: object, responseType: 'sync' | 'query'|'execute'| 'disconnect') {
+  if (responseType == 'sync') {
+    return validateSyncResponse(apiResponse);
+  }
+  return undefined;
 }
