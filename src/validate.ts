451e862a0d4ac140d86f7a784b1b12226f7744fb
jolene-loz <lozanoja@bc.edu>
2020-08-14 11:00:32 -0400

    Removed helper func, changed schemas to module level

--- src/validate.ts
+++ src/validate.ts
@@ -5,22 +5,17 @@ const ajv = new Ajv({
   allErrors: true,
 });
 
-
-/**
- * Helper function that throws an error with a custom message
- * @param message Error message defined 
- * @return Throws an error with a message 
- */
-function throwError(message: string): never {
-    throw new Error(message);
-}
+const syncResponseSchema = require('../intents/sync.response.schema.json');
+const queryResponseSchema = require('../intents/query.response.schema.json');
+const executeResponseSchema = require('../intents/execute.response.schema.json');
+const disconnectResponseSchema = require('../intents/disconnect.response.schema.json');
 
 /**
  * Helper function that uses AJV library to validate the response against the schema
  * @param apiResponse User defined api response
  * @return Returns undefined if valid is true, returns an array of error(s) if valid is false.
  */
-function responseValidation(apiResponse: object, schema: object){
+function responseValidation(apiResponse: object, schema: object) {
   const isValid = ajv.validate(schema, apiResponse);
   if (isValid) {
     return undefined;
@@ -35,20 +30,14 @@ function responseValidation(apiResponse: object, schema: object){
  * @return Errors from AJV validation, if any. Undefined otherwise.
  */
 export function validate(apiResponse: object, responseType: 'sync' | 'query'|'execute'| 'disconnect') {
-    
-  const syncResponseSchema = require('../intents/sync.response.schema.json');
-  const queryResponseSchema = require('../intents/query.response.schema.json');
-  const executeResponseSchema = require('../intents/execute.response.schema.json');
-  const disconnectResponseSchema = require('../intents/disconnect.response.schema.json');    
-  
   if (responseType === 'sync') {
-    return responseValidation(apiResponse,syncResponseSchema);
-  } else if (responseType == 'query'){
-    return responseValidation(apiResponse,queryResponseSchema);
-  } else if (responseType == 'execute'){
-    return responseValidation(apiResponse,executeResponseSchema);      
-  } else if (responseType == 'disconnect'){
-    return responseValidation(apiResponse,disconnectResponseSchema);      
+    return responseValidation(apiResponse, syncResponseSchema);
+  } else if (responseType == 'query') {
+    return responseValidation(apiResponse, queryResponseSchema);
+  } else if (responseType == 'execute') {
+    return responseValidation(apiResponse, executeResponseSchema);
+  } else if (responseType == 'disconnect') {
+    return responseValidation(apiResponse, disconnectResponseSchema);
   }
-  return throwError('Response type not specified');
+  throw new Error('Response type not specified');
 }
