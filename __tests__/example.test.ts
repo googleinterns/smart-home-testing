6e0c63f92f9d82eaefc05818c17c8279d59671bb
jolene-loz <lozanoja@bc.edu>
2020-08-14 17:20:35 -0400

    Syntax revisions

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -1,28 +1,31 @@
 'use-strict';
 import * as testlib from '../src/validate';
 import * as testreq from '../src/requests';
-
 const {smarthome} = require('actions-on-google');
-const app = smarthome();
+
+const app = smarthome({
+  debug: true,
+});
+
 // Test passes
-test('Test 1: Sync Response passes', async () => {
+test(Sync Response passes', async () => {
   const exampleSyncRes = require('./example.sync.response.json');
   const noErrors = testlib.validate(exampleSyncRes, 'sync');
   expect(noErrors).toBe(undefined);
 });
 
-// Test fails and shows errors
-test('Test 2: Sync Response fails', async () => {
-  const exampleSyncResFail = require('./example.sync.response.fail.json');
-  const syncErrors = testlib.validate(exampleSyncResFail, 'sync');
-  console.log(syncErrors);
-  expect(syncErrors && syncErrors.join('\n')).toBe(undefined);
-});
-
-test('Test 3: Sync Response using actions an intent handler', async () =>{
+test('Sync Response using actions an intent handler', async () =>{
   const reqSync = testreq.generateSyncRequest();
-  const res = app.handler(reqSync);
+  const res = app.onSync(reqSync)
   const testlibValid = testlib.validate(res, 'sync');
+  console.log(testlibValid)
   expect(testlibValid).toBe(undefined);
 });
 
+// Test fails and shows errors
+test('Sync Response fails', async () => {
+  const exampleSyncResFail = require('./example.sync.response.fail.json');
+  const syncErrors = testlib.validate(exampleSyncResFail, 'sync');
+  console.log(syncErrors);
+  expect(syncErrors && syncErrors.join('\n')).toBeFalsy();
+});
