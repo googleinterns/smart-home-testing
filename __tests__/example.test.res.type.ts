aa4ce69b95099cd866baf6e5df24539f0147a95c
jolene-loz <lozanoja@bc.edu>
2020-08-17 11:20:21 -0400

    Changes to throw syntax to match jest

--- __tests__/example.test.res.type.ts
+++ __tests__/example.test.res.type.ts
@@ -5,5 +5,5 @@ test('Sync Response fails due to invalid response type', async () => {
   const exampleSyncRes = require('./example.sync.response.json');
   const syncErrors = testlib.validate(exampleSyncRes, 'fail');
   console.log(syncErrors);
-  expect(syncErrors && syncErrors.join('\n')).to.throw();
+  expect(syncErrors && syncErrors.join('\n')).toThrow(TypeError);
 });
