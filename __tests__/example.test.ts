826f3d028f22682459492212ea38927d1beb71f0
jolene-loz <lozanoja@bc.edu>
2020-08-19 16:50:17 -0400

    Added the user defined pass and fail tests

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -11,4 +11,21 @@ describe.only('Sync response testing suite', () => {
     const testlibValid = testlib.validate(reqSync, res);
     expect(testlibValid).toBe(undefined);
   });
+
+  // Test fails defined user sync response 
+  test('Sync Response using a given sync response fails', async () =>{
+    const reqSync = testreq.generateSyncRequest();
+    const res = require('./example.sync.response.fail.json');
+    const testlibValid = testlib.validate(reqSync, res);
+    expect(testlibValid).not.toBe(undefined);
+  });
+
+  // Test passes defined user sync response 
+  test('Sync Response using a given sync response passes', async () =>{
+    const reqSync = testreq.generateSyncRequest();
+    const res = require('./example.sync.response.json');
+    const testlibValid = testlib.validate(reqSync, res);
+    expect(testlibValid).toBe(undefined);
+  });
+  
 });
