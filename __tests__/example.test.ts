b8597bd87dabdf79539eb8076f932f37b8c1b0d0
jolene-loz <lozanoja@bc.edu>
2020-08-21 18:37:04 -0400

    Readded trait validations and tests

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -11,10 +11,8 @@ describe.only('Sync response testing suite', () => {
     const testlibValid = testlib.validate(reqSync, res);
     expect(testlibValid).toBe(undefined);
   });
-
-  // Test fails defined user sync response 
+  // Test fails defined user sync response
   test('Sync Response using a given sync response fails', async () =>{
-    const reqSync = testreq.generateSyncRequest();
     const res = require('./example.sync.response.fail.json');
     const testlibValid = testlib.validate(reqSync, res);
     expect(testlibValid).not.toBe(undefined);
@@ -22,9 +20,54 @@ describe.only('Sync response testing suite', () => {
 
   // Test passes defined user sync response 
   test('Sync Response using a given sync response passes', async () =>{
-    const reqSync = testreq.generateSyncRequest();
     const res = require('./example.sync.response.json');
     const testlibValid = testlib.validate(reqSync, res);
+    expect(testlibValid).toBe(undefined);
+  });
+});
+
+const devices = [{
+  'id': '123',
+  'customData': {
+    'fooValue': 74,
+    'barValue': true,
+    'bazValue': 'foo',
+  },
+}];
+
+describe.only('Query response testing suite', () => {
+  // Test passes
+  test('Query Response using actions an intent handler', async () =>{
+    const reqQuery = testreq.generateQueryRequest(devices);
+    const res = fakeapp.onQuery(reqQuery);
+    const testlibValid = testlib.validate(reqQuery, res);
+    expect(testlibValid).toBe(undefined);
+  });
+});
+
+const execution = [{
+  'command': 'action.devices.commands.OnOff',
+  'params': {
+    'on': true,
+  },
+}];
+
+describe.only('Execute response testing suite', () => {
+  // Test should pass but there is an error here right now.
+  test('Execute Response using actions an intent handler', async () =>{
+    const reqExecute = testreq.generateExecuteRequest(devices, execution);
+    const res = fakeapp.onExecute(reqExecute);
+    const testlibValid = testlib.validate(reqExecute, res);
+    expect(testlibValid).toBe(undefined);
+  });
+});
+
+describe.only('Disconnect response testing suite', () => {
+  // Test should pass but there is an error here right now.
+  test('Disconnect Response using actions an intent handler', async () =>{
+    const reqDisconnect = testreq.generateDisconnectRequest();
+    const res = fakeapp.onDisconnect(reqDisconnect);
+    const testlibValid = testlib.validate(reqDisconnect, res);
     expect(testlibValid).toBe(undefined);
   });
   
--- src/validate.ts
+++ src/validate.ts
@@ -9,7 +9,16 @@ const SYNC_RESPONSE_SCHEMA = require('../intents/sync.response.schema.json');
 const QUERY_RESPONSE_SCHEMA = require('../intents/query.response.schema.json');
 const EXECUTE_RESPONSE_SCHEMA = require('../intents/execute.response.schema.json');
 const DISCONNECT_RESPONSE_SCHEMA = require('../intents/disconnect.response.schema.json');
+const ON_OFF_STATES_SCHEMA = require('../traits/onoff.states.schema.json');
+const ON_OFF_ATTRIBUTES_SCHEMA = require('../traits/onoff.attributes.schema.json');
 
+const COMMAND_STATES_EXPECT = {
+  'action.devices.commands.OnOff': ON_OFF_STATES_SCHEMA,
+};
+
+const TRAIT_ATTRIBUTES_EXPECT = {
+  'action.devices.traits.OnOff': ON_OFF_ATTRIBUTES_SCHEMA,
+};
 /**
  * Helper function that uses AJV library to validate the response against the schema
  * @param apiResponse User defined api response
@@ -26,18 +35,75 @@ function responseValidation(apiResponse: object, schema: object) {
 
 /**
  * Identifies the response type and validates the function based on the schemas.
- * @param apiResponse User defined api response
- * @param responseType User defined intent response
+ * @param req Request from one of the generate request functions.
+ * @param apiResponse User defined api response.
  * @return Errors from AJV validation, if any. Undefined otherwise.
  */
 export function validate(intentRequest: object, apiResponse: object) {
   const responseType = intentRequest['inputs'][0]['intent'];
   if (responseType === 'action.devices.SYNC') {
-    return responseValidation(apiResponse, SYNC_RESPONSE_SCHEMA);
+    // validate traits with attributes schema
+    const syncErrors : object[] = [];
+    const syncDevices = apiResponse['payload']['devices'];
+    const syncDevicesLength = syncDevices.length;
+    for (let i = 0; i < syncDevicesLength; i++) {
+      const traits = syncDevices[i]['traits'];
+      const traitsLength = syncDevices[i]['traits'].length;
+      const attributes = syncDevices[i]['attributes'];
+      if (attributes === undefined){
+          return responseValidation(apiResponse, SYNC_RESPONSE_SCHEMA);
+      }
+      for (let j = 0; j < traitsLength; j++) {
+        const trait = traits[j];
+        if (trait in TRAIT_ATTRIBUTES_EXPECT) {
+          const validateTraitRes = responseValidation(attributes, TRAIT_ATTRIBUTES_EXPECT[trait]);
+          if (validateTraitRes) {
+            return syncErrors.push(...validateTraitRes);
+          } 
+        }
+      }
+    }
+    return syncErrors.length ? syncErrors : undefined; 
   } else if (responseType === 'action.devices.QUERY') {
-    return responseValidation(apiResponse, QUERY_RESPONSE_SCHEMA);
+    // validate with states schema
+    const queryErrors : object[] = [];
+    const devices = intentRequest['inputs'][0]['payload']['devices'];
+    const devicesLength = devices.length;
+    for (let i = 0; i < devicesLength; i++) {
+      const deviceIds = devices[i]['id'];
+      const states = apiResponse['payload']['devices'][deviceIds];
+      // still working on this function figuring out a way to not hardcode the action in here
+      const validateQueryTraitStates = responseValidation(states, COMMAND_STATES_EXPECT['action.devices.commands.OnOff']);
+      if (validateQueryTraitStates) {
+        return queryErrors.push(...validateQueryTraitStates);
+      } 
+    }
+    return queryErrors.length ? queryErrors : undefined; 
   } else if (responseType === 'action.devices.EXECUTE') {
-    return responseValidation(apiResponse, EXECUTE_RESPONSE_SCHEMA);
+    // validate with states schema
+    const executeErrors : object[] = [];
+    // gets the specific command
+    const execution = intentRequest['inputs'][0]['payload']['execution'];
+    const executionLength = intentRequest['inputs'][0]['payload']['execution'].length;
+
+    // identifies the part of the api response to validate against a schema
+    const commands = apiResponse['payload']['commands'];
+    const commandsLength = commands.length;
+
+    for (let i = 0; i < executionLength; i++) {
+      const commandName = execution[i]['command'];
+      if (commandName in COMMAND_STATES_EXPECT) {
+        for (let j = 0; j < commandsLength; j++) {
+          const states = commands[j]['states'];
+          const validateExecTraitStates = responseValidation(states, COMMAND_STATES_EXPECT[commandName]);
+          if (validateExecTraitStates) {
+            return executeErrors.push(...validateExecTraitStates);
+          } else {
+            return responseValidation(apiResponse, EXECUTE_RESPONSE_SCHEMA);
+          }
+        }
+      }
+    }
   } else if (responseType === 'action.devices.DISCONNECT') {
     return responseValidation(apiResponse, DISCONNECT_RESPONSE_SCHEMA);
   } throw new Error('Response type not valid');
