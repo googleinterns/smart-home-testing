f1798f58e97c9a38cf958d1ef7afe24171a0d699
jolene-loz <lozanoja@bc.edu>
2020-08-26 12:22:29 -0400

    Code style changes to SYNC

--- src/validate.ts
+++ src/validate.ts
@@ -46,38 +46,44 @@ function responseValidation(apiResponse: object, schema: object) {
  */
 export function validate(intentRequest: object, apiResponse: object, syncData?: object){
   const responseType = intentRequest['inputs'][0]['intent'];
+
   if (responseType === 'action.devices.SYNC') {
     const validateSyncAPI = responseValidation(apiResponse, SYNC_RESPONSE_SCHEMA); 
     const syncErrors : object[] = [];
-    if (validateSyncAPI === undefined){
-        const syncDevices = apiResponse['payload']['devices'];
-        const syncDevicesLength = syncDevices.length;
-        for (let i = 0; i < syncDevicesLength; i++) {
-          const traits = syncDevices[i]['traits'];
-          const traitsLength = syncDevices[i]['traits'].length;
-          const attributes = syncDevices[i]['attributes'];
-          if (attributes === undefined) {
-            return validateSyncAPI;
-          }
-          for (let j = 0; j < traitsLength; j++) {
-            const trait = traits[j];
-            if (trait in TRAIT_ATTRIBUTES_EXPECT) {
-              const validateTraitRes = responseValidation(attributes, TRAIT_ATTRIBUTES_EXPECT[trait]);
-              if (validateTraitRes) {
-                syncErrors.push(...validateTraitRes);
-              }
-            }
-          }
-       } 
-    } else {
-        syncErrors.push(...validateSyncAPI!);
+    
+    if (validateSyncAPI) {
+        syncErrors.push(...validateSyncAPI);
+        return syncErrors;
     }
+
+    const syncDevices = apiResponse['payload']['devices'];
+    const syncDevicesLength = syncDevices.length;
+    for (let i = 0; i < syncDevicesLength; i++) {
+      const traits = syncDevices[i]['traits'];
+      const traitsLength = syncDevices[i]['traits'].length;
+      const attributes = syncDevices[i]['attributes'];
+      if (attributes === undefined) {
+        return validateSyncAPI;
+      }
+      for (let j = 0; j < traitsLength; j++) {
+        const trait = traits[j];
+        if (trait in TRAIT_ATTRIBUTES_EXPECT) {
+          const validateTraitRes = responseValidation(attributes, TRAIT_ATTRIBUTES_EXPECT[trait]);
+          if (validateTraitRes) {
+            syncErrors.push(...validateTraitRes);
+          }
+        }
+      }
+   } 
     return syncErrors.length ? syncErrors : undefined;
   } else if (responseType === 'action.devices.QUERY') {
     // validate with states schema;
     const queryErrors : object[] = [];
     const devices = intentRequest['inputs'][0]['payload']['devices'];
     const devicesLength = devices.length;
+    
+    const validateQueryAPI = responseValidation(apiResponse, QUERY_RESPONSE_SCHEMA);
+    
     for (let i = 0; i < devicesLength; i++) {
       const deviceIds = devices[i]['id'];
       const states = apiResponse['payload']['devices'][deviceIds];
@@ -95,7 +101,7 @@ export function validate(intentRequest: object, apiResponse: object, syncData?: 
           }
         }
       }
-    return responseValidation(apiResponse, QUERY_RESPONSE_SCHEMA);
+    return queryErrors.length ? queryErrors : undefined;
   } else if (responseType === 'action.devices.EXECUTE') {
     // validate with states schema
     const executeErrors : object[] = [];
