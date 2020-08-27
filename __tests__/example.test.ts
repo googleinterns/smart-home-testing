cdd0b10912b83396e40fa18a1ed349476c741d46
jolene-loz <lozanoja@bc.edu>
2020-08-27 13:54:51 -0400

    Committing tests

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -3,6 +3,8 @@ import * as testlib from '../src/validate';
 import * as testreq from '../src/requests';
 import * as fakeapp from './fake-app';
 
+const requestSync = testreq.generateSyncRequest();
+const responseSync = fakeapp.onSync(requestSync);
 describe.only('Sync response testing suite', () => {
   const requestSync = testreq.generateSyncRequest();
   const responseSync = fakeapp.onSync(requestSync);
@@ -20,6 +22,7 @@ describe.only('Sync response testing suite', () => {
 
   // Test passes defined user sync response
   test('Sync Response using a given sync response passes', async () =>{
+<<<<<<< Updated upstream
     const responseSync = require('./example.sync.response.json');
     const testlibValid = testlib.validate(requestSync, responseSync);
     expect(testlibValid).toBe(undefined);
@@ -90,6 +93,80 @@ describe.only('Disconnect response testing suite', () => {
     const requestDisconnect = testreq.generateDisconnectRequest();
     const responseDisconnect = fakeapp.onDisconnect(requestDisconnect);
     const testlibValid = testlib.validate(requestDisconnect, responseDisconnect);
+||||||| merged common ancestors
+    const reqSync = testreq.generateSyncRequest();
+    const res = require('./example.sync.response.json');
+    const testlibValid = testlib.validate(reqSync, res);
+=======
+    const responseSync = require('./example.sync.response.json');
+    const testlibValid = testlib.validate(requestSync, responseSync);
+    expect(testlibValid).toBe(undefined);
+  });
+});
+
+const devices = [{
+  'id': '123',
+  'customData': {
+    'fooValue': 74,
+    'barValue': true,
+    'bazValue': 'foo',
+  },
+}, {
+  'id': '456',
+  'customData': {
+    'fooValue': 12,
+    'barValue': false,
+    'bazValue': 'bar',
+  },
+}];
+
+const requestQuery = testreq.generateQueryRequest(devices);
+
+describe.only('Query response testing suite', () => {
+  // Test passes
+  test('Query Response passes using actions an intent handler', async () =>{
+    const responseQuery = fakeapp.onQuery(requestQuery);
+    const testlibValid = testlib.validate(requestQuery, responseQuery, responseSync);
+    expect(testlibValid).toBe(undefined);
+  });
+
+  test('Query Response fails using actions a given query response', async () =>{
+    const responseQuery = require('./example.query.response.fail.json');
+    const testlibValid = testlib.validate(requestQuery, responseQuery, responseSync);
+    expect(testlibValid).not.toBe(undefined);
+  });
+});
+
+const execution = [{
+  'command': 'action.devices.commands.OnOff',
+  'params': {
+    'on': true,
+  },
+}];
+const requestExecute = testreq.generateExecuteRequest(devices, execution);
+
+describe.only('Execute response testing suite', () => {
+  // Test should pass but there is an error here right now.
+  test('Execute Response using actions an intent handler', async () =>{
+    const responseExecute = fakeapp.onExecute(requestExecute);
+    const testlibValid = testlib.validate(requestExecute, responseExecute);
+    expect(testlibValid).toBe(undefined);
+  });
+
+  test('Execute Response fails using actions a given execute response', async () =>{
+    const responseExecute = require('./example.execute.response.fail.json');
+    const testlibValid = testlib.validate(requestExecute, responseExecute);
+    expect(testlibValid).not.toBe(undefined);
+  });
+});
+
+describe.only('Disconnect response testing suite', () => {
+  // Test should pass but there is an error here right now.
+  test('Disconnect Response using actions an intent handler', async () =>{
+    const requestDisconnect = testreq.generateDisconnectRequest();
+    const responseDisconnect = fakeapp.onDisconnect(requestDisconnect);
+    const testlibValid = testlib.validate(requestDisconnect, responseDisconnect);
+>>>>>>> Stashed changes
     expect(testlibValid).toBe(undefined);
   });
 });
