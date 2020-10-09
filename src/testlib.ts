964509122261ae48644b80d985f20fa0aedfd2ad
Dave Smith <smithdave@google.com>
2020-10-09 12:18:00 -0600

    Centralize schema loader. Clean up constants.

--- src/testlib.ts
+++ src/testlib.ts
@@ -7,17 +7,30 @@ const ajv = new Ajv({
   allErrors: true,
 });
 
+enum SchemaType {
+  INTENTS = 'intents',
+  TRAITS = 'traits',
+}
+
+enum PayloadType {
+  REQUEST = 'request',
+  RESPONSE = 'response',
+  ATTRIBUTES = 'attributes',
+  STATES = 'states',
+  PARAMS = 'params',
+  RESULTS = 'results',
+}
+
 /**
  * Retrieve the JSON schema for the given intent.
  * @param intentName FQN of the intent
- * @param schemaType One of 'request' or 'response'
+ * @param payloadType One of 'request' or 'response'
  */
-function getIntentSchema(intentName: string, schemaType: string): object | undefined {
+function getIntentSchema(intentName: string, payloadType: PayloadType): object | undefined {
   try {
     const shortIntent = intentName.split('.').pop()?.toLowerCase() || 'unknown';
-    const filename = `${shortIntent}.${schemaType}.schema.json`;
-    const basePath = path.join(__dirname, '../schema/intents');
-    return require(path.join(basePath, shortIntent, filename));
+    const filename = `${shortIntent}.${payloadType}.schema.json`;
+    return requireSchema(SchemaType.INTENTS, shortIntent, filename);
   } catch (error) {
     console.error(error);
     return undefined;
@@ -27,14 +40,13 @@ function getIntentSchema(intentName: string, schemaType: string): object | undef
 /**
  * Retrieve the JSON schema for the given device trait.
  * @param traitName FQN of the device trait
- * @param schemaType One of 'attributes' or 'states'
+ * @param payloadType One of 'attributes' or 'states'
  */
-function getTraitSchema(traitName: string, schemaType: string): object | undefined {
+function getTraitSchema(traitName: string, payloadType: PayloadType): object | undefined {
   try {
     const shortTrait = traitName.split('.').pop()?.toLowerCase() || 'unknown';
-    const filename = `${shortTrait}.${schemaType}.schema.json`;
-    const basePath = path.join(__dirname, '../schema/traits');
-    return require(path.join(basePath, shortTrait, filename));
+    const filename = `${shortTrait}.${payloadType}.schema.json`;
+    return requireSchema(SchemaType.TRAITS, shortTrait, filename);
   } catch (error) {
     console.error(traitName, error);
     return undefined;
@@ -44,15 +56,30 @@ function getTraitSchema(traitName: string, schemaType: string): object | undefin
 /**
  * Retrieve JSON schema for the given device command
  * @param commandName FQN of the device command
- * @param schemaType One of 'params' or 'results'
+ * @param payloadType One of 'params' or 'results'
  */
-function getCommandSchema(commandName: string, schemaType: string): object| undefined {
+function getCommandSchema(commandName: string, payloadType: PayloadType): object| undefined {
   try {
-    const shortTrait = findEnclosingTrait(commandName, schemaType) || 'unknown';
+    const shortTrait = findEnclosingTrait(commandName, payloadType) || 'unknown';
     const shortCommand = commandName.split('.').pop()?.toLowerCase() || 'unknown';
-    const filename = `${shortCommand}.${schemaType}.schema.json`;
-    const basePath = path.join(__dirname, '../schema/traits');
-    return require(path.join(basePath, shortTrait, filename));
+    const filename = `${shortCommand}.${payloadType}.schema.json`;
+    return requireSchema(SchemaType.TRAITS, shortTrait, filename);
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+/**
+ * Helper function to load the target schema file from schema set
+ * @param schemaType Schema directory path ('intents' or 'traits')
+ * @param targetName Intent or trait name enclosing the schema
+ * @param filename Schema file to load
+ */
+function requireSchema(schemaType: SchemaType, targetName: string, filename: string) {
+  try {
+    const basePath = path.join(__dirname, '../schema', schemaType);
+    return require(path.join(basePath, targetName, filename));
   } catch (error) {
     console.error(error);
     return undefined;
@@ -61,8 +88,8 @@ function getCommandSchema(commandName: string, schemaType: string): object| unde
 
 /**
  * Helper function to search for matching trait directory
- * @param commandName
- * @param schemaType
+ * @param commandName FQN of the device command
+ * @param schemaType One of 'params' or 'results'
  */
 function findEnclosingTrait(commandName: string, schemaType: string) {
   try {
@@ -102,7 +129,7 @@ function responseValidation(apiResponse: object, schema: object) {
 export function validate(intentRequest: object, apiResponse: object, syncData?: object) {
   const responseType = intentRequest['inputs'][0]['intent'];
 
-  const responseSchema = getIntentSchema(responseType, 'response');
+  const responseSchema = getIntentSchema(responseType, PayloadType.RESPONSE);
   if (responseType === 'action.devices.SYNC' && responseSchema) {
     const validateSyncAPI = responseValidation(apiResponse, responseSchema);
     const syncErrors : object[] = [];
@@ -119,7 +146,7 @@ export function validate(intentRequest: object, apiResponse: object, syncData?: 
       const attributes = syncDevices[i]['attributes'] || {};
       for (let j = 0; j < traits.length; j++) {
         const trait = traits[j];
-        const schema = getTraitSchema(trait, 'attributes');
+        const schema = getTraitSchema(trait, PayloadType.ATTRIBUTES);
         if (schema) {
           const validateTraitRes = responseValidation(attributes, schema);
           if (validateTraitRes) {
@@ -150,7 +177,7 @@ export function validate(intentRequest: object, apiResponse: object, syncData?: 
         const traits = device['traits'];
           for (let j = 0; j < traits.length; j++) {
             const trait = traits[j];
-            const schema = getTraitSchema(trait, 'states');
+            const schema = getTraitSchema(trait, PayloadType.STATES);
             if (schema) {
               const validateQueryTraitStates = responseValidation(states, schema);
               if (validateQueryTraitStates) {
@@ -182,7 +209,7 @@ export function validate(intentRequest: object, apiResponse: object, syncData?: 
       // gets the specific command
       const commandName = execution[i]['command'];
       const traitName = findEnclosingTrait(commandName, 'params') || 'unknown';
-      const schema = getTraitSchema(traitName, 'states');
+      const schema = getTraitSchema(traitName, PayloadType.STATES);
       if (schema) {
         for (let j = 0; j < commandsLength; j++) {
           const states = commands[j]['states'] || {};
